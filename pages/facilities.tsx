03a2f2cda71cae9d0b30cc69ef27626d649856d7
Ragavi14 <ragavi@netiapps.com>
2024-01-01 11:55:06 +0530

    changes

---
+++ pages/facilities.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navigation from "../components/navigation/navigation";
+import FooterSection from "../components/footerSection/footerSection";
+import InnerBannerSection from "../components/innerBannerSection/innerBannerSection";
+import FacilitySection from "../components/facilitieSection/facilitySection";
+
+export default function Page() {
+  return (
+      <div>
+          <Navigation/>
+          <InnerBannerSection/>
+          <FacilitySection/>
+
+          <FooterSection/>
+      </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../styles/global.scss';
+import Navigation from "../components/navigation/navigation";
+import HeroSection from "../components/heroSection/heroSection";
+import AboutSection from "../components/aboutSection/aboutSection";
+import ServiceSection from "../components/serviceSection/serviceSection";
+import SectorSection from "../components/sectorSection/sectorSection";
+import ClientSection from "../components/clientSection/clientSection";
+import CertificateSection from "../components/certificateSection/certificateSection";
+import FooterSection from "../components/footerSection/footerSection";
+
+export default function Home() {
+  return (
+      <div>
+          <Navigation/>
+          <HeroSection/>
+          <AboutSection/>
+          <ServiceSection/>
+          <SectorSection/>
+          <ClientSection/>
+          <CertificateSection/>
+          <FooterSection/>
+      </div>
+  )
+}
