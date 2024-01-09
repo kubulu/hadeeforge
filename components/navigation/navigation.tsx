f912fee245468636b62b2628c6fc0c153fb16703
Ragavi14 <ragavi@netiapps.com>
2024-01-09 17:52:54 +0530

    changes

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './navigation.module.scss'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Navigation(nav: any) {
-// console.log('NAV=',nav);
+console.log('NAV=',nav);
+
+const [openDropdownIndex, setOpenDropdownIndex] = useState(-1);
+
+const handleDropdownOpen = (index: number) => {
+    setOpenDropdownIndex(index);
+};
+
+const handleDropdownClose = () => {
+    setOpenDropdownIndex(-1);
+};
+
     return (
        <div>
            <div className="container-fluid">
@@ -13,9 +27,32 @@ export default function Navigation(nav: any) {
                             <img src={nav.nav.logo.logo_img} alt="logo"/>
                        </a>
                    </div>
+<<<<<<< HEAD
                 {nav.nav.nav_item.map((element: any, index: any)=>(
                    <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                        <li key={index}> <a href={element.list_item.link} className="nav-link px-4 link-secondary"> {element.list_item.name} </a></li>
+=======
+                {nav.nav.nav_item?.map((element: any, index: any)=>(
+                   <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" key={index}>
+                       <li>
+                        {/* <a href={element.list_item.link} className="nav-link px-4 link-secondary"> {element.list_item.name} </a> */}
+                        {element.list_item.sub_menu ?
+                        
+                        <NavDropdown title={element.list_item.name} show={openDropdownIndex === index}
+                                                    onMouseEnter={() => handleDropdownOpen(index)}
+                                                    className="nav-link px-4 link-secondary"
+                                                    onMouseLeave={handleDropdownClose} >
+                                                        {element.list_item.sub_menu.map((ele: any, ind: any)=>(
+                                                        <NavDropdown.Item key={ind} href={ele.link}> {ele.name} </NavDropdown.Item>
+                                                        ))}
+
+                        </NavDropdown>
+                        :   
+                         <a href={element.list_item.link} className="nav-link px-4 link-secondary"> {element.list_item.name} </a>
+
+                        }
+                        </li>
+>>>>>>> 2a3ed02 (changes)
                    </ul>
                  ))}
                    <div className="col-md-3 text-end">
