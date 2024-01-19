1580f8077093a092bc8b0d283e72e2514ad6f8a5
shashi <shashi09568@gmail.com>
2024-01-19 16:54:41 +0530

    changes

--- components/productDetailSection/productDetailsSection.tsx
+++ components/productDetailSection/productDetailsSection.tsx
@@ -42,13 +42,13 @@ export default function ProductDetailsSection(productsDetails: any) {
     return (
       <div className={styles.proDetails}>
           <div className={`container`}>
-              <div className={`row`}>
-                  <div className={`col-12`}>
-                      <div className={styles.proHeadingText}>
-                          <h3> {productsDetails.productsDetails.heading_text} </h3>
-                      </div>
-                  </div>
-              </div>
+              {/*<div className={`row`}>*/}
+              {/*    <div className={`col-12`}>*/}
+              {/*        <div className={styles.proHeadingText}>*/}
+              {/*            <h3> {productsDetails.productsDetails.heading_text} </h3>*/}
+              {/*        </div>*/}
+              {/*    </div>*/}
+              {/*</div>*/}
               <div className={`row`}>
                   <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                       <li className="nav-item" role="presentation">
@@ -66,12 +66,21 @@ export default function ProductDetailsSection(productsDetails: any) {
                                   type="button" role="tab" aria-controls="contact" aria-selected="false">Industrial Parts
                           </button>
                       </li>
+                      <li className="nav-item" role="presentation">
+                          <button className={`nav-link ${industrialTab}`} id="industrial-tab" data-bs-toggle="tab" data-bs-target="#others"
+                                  type="button" role="tab" aria-controls="contact" aria-selected="false">Others
+                          </button>
+                      </li>
 
                   </ul>
                   <div className="tab-content" id="myTabContent">
 
                       <div className={`tab-pane fade ${automotiveContent}`} id="automotive" role="tabpanel" aria-labelledby="automotive-tab">
                         <div className={`row mt-5 align-items-center justify-content-center`}>
+                            <div className={`w-100 text-center`}>
+                                <p> {productsDetails.productsDetails.heading_text} </p>
+                            </div>
+                            <h4> Steel Parts</h4>
                                 {productsDetails.productsDetails.automotive_parts.map((element: any, index: any)=>(
                                     <div className={`col-md-2`} key={index}>
                                         <div className={styles.proImg} data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => openModal(element.img)}>
@@ -81,11 +90,22 @@ export default function ProductDetailsSection(productsDetails: any) {
                                     </div>
 
                                     ))}
+                            <h4>Aluminum Parts</h4>
+                            {productsDetails.productsDetails.automotive_parts.map((element: any, index: any)=>(
+                                <div className={`col-md-2`} key={index}>
+                                    <div className={styles.proImg} data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => openModal(element.img)}>
+                                        <img  src={element.img} />
+                                    </div>
+
+                                </div>
+
+                            ))}
                         </div>
                     </div>
 
                       <div className={`tab-pane fade ${aerospaceContent}`} id="aerospace" role="tabpanel" aria-labelledby="aerospace-tab">
                         <div className={`row mt-5 align-items-center justify-content-center`}>
+                            <h4>Aluminum Parts</h4>
                                 {productsDetails.productsDetails.aerospace_parts.map((element: any, index: any)=>(
                                     <div className={`col-md-2`} key={index}>
                                         <div className={styles.proImg} data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => openModal(element.img)}>
@@ -95,6 +115,16 @@ export default function ProductDetailsSection(productsDetails: any) {
                                     </div>
 
                                     ))}
+                            <h4>Stainless Steel Parts</h4>
+                            {productsDetails.productsDetails.aerospace_parts.map((element: any, index: any)=>(
+                                <div className={`col-md-2`} key={index}>
+                                    <div className={styles.proImg} data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => openModal(element.img)}>
+                                        <img  src={element.img} />
+                                    </div>
+
+                                </div>
+
+                            ))}
                         </div>
                     </div>
 
@@ -111,6 +141,19 @@ export default function ProductDetailsSection(productsDetails: any) {
                                     ))}
                         </div>
                     </div>
+                      <div className={`tab-pane fade ${industrialContent}`} id="others" role="tabpanel" aria-labelledby="others-tab">
+                          <div className={`row mt-5 align-items-center justify-content-center`}>
+                              {productsDetails.productsDetails.industrial.map((element: any, index: any)=>(
+                                  <div className={`col-md-2`} key={index}>
+                                      <div className={styles.proImg} data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => openModal(element.img)}>
+                                          <img  src={element.img} />
+                                      </div>
+
+                                  </div>
+
+                              ))}
+                          </div>
+                      </div>
                     <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
                       <div className="modal-dialog">
                         <div className="modal-content">
