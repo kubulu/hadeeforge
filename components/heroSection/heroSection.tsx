ed22c2684a872e8b8d46a92f63e2f9782b476828
shashi <shashi09568@gmail.com>
2024-01-19 13:55:08 +0530

    changes

--- components/heroSection/heroSection.tsx
+++ components/heroSection/heroSection.tsx
@@ -9,7 +9,7 @@ export default function HeroSection(hero: any) {
     // console.log('Banner=',hero);
 
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         autoplay: true,
         speed: 500,
@@ -19,30 +19,40 @@ export default function HeroSection(hero: any) {
     };
 
     return (
-       <div className="" >
+       <div className={`${styles.banner}`} >
+           <div className={`row`}>
+               <div className={`col-md-6 p-5 justi`}>
+                   <div className={`d-flex justify-content-center align-items-center h-100`}>
+                       <div>
+                           <h5>Welcome to HaDee forgings</h5>
+                           <h2>Providing Chip less Solutions to Automotive, Aerospace, Electricals Domestic appliance industries.</h2>
+                       </div>
+                   </div>
+               </div>
+               <div className={`col-md-6`}>
                    <Slider {...settings}>
-                        {hero.hero.map((element: any, index: any)=>(
-                            <div className="carousel-item active"  key={index}>
-                                <div className={`${styles.banner}`}>
-                                    <div className={``}>
-                                        <div className={`row justify-content-center align-items-center`}>
-                                            <div className={`col-md-6 px-5`}>
-                                                <div dangerouslySetInnerHTML={{ __html: element.text }} />
-                                                <button className={`btn btn-outline-light`}> {element.button_name} </button>
-                                            </div>
-                                            <div className={`col-md-6`}>
-                                                <div className={styles.bannerImg}>
-                                                    <img src={element.img} alt="images" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                        </Slider>
-                   {/* </div> */}
-               {/* </div> */}
+                       {hero.hero.map((element: any, index: any)=>(
+                           <div className="carousel-item active"  key={index}>
+                               <div>
+                                   <div className={``}>
+                                       <div className={`row justify-content-center align-items-center px-0`}>
+                                           {/*<div className={`col-md-6 px-5`}>*/}
+                                           {/*    <div dangerouslySetInnerHTML={{ __html: element.text }} />*/}
+                                           {/*    <button className={`btn btn-outline-light`}> {element.button_name} </button>*/}
+                                           {/*</div>*/}
+                                           <div className={`col-md-12`}>
+                                               <div className={styles.bannerImg}>
+                                                   <img src={element.img} alt="images" />
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+                       ))}
+                   </Slider>
+               </div>
+           </div>
        </div>
     )
 }
