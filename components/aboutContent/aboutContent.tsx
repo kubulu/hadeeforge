a51f1643afee2f4b446742e46952326044f4dbd0
shashi <shashi09568@gmail.com>
2024-01-19 14:51:22 +0530

    changes

--- components/aboutContent/aboutContent.tsx
+++ components/aboutContent/aboutContent.tsx
@@ -6,13 +6,16 @@ export default function AboutContent(aboutContent: any) {
     return (
        <div className="container-fluid">
            <div className={styles.aboutContent}>
-                <div className={`row px-0`}>
-                    <div className={`col-8 pt-5 pb-5`}>
+                <div className={`row px-0 justify-content-center align-items-center`}>
+                    <div className={`col-md-8 pt-5 pb-5`}>
                         <h3> {aboutContent.aboutContent.text} </h3>
                         <h3>HADEE FORGINGS and GUNMA SEIKO CO., LTD. (Japan) have a technical support contract for forging, machining and quality.</h3>
                         <h3>The unit is ISO 9001-2015 & IATF 16949 certified Company.</h3>
                         <h3>We are approved by Maruthi Suzuki as a green channel supplier</h3>
                         <h3>All our furnaces are calibrated as per CQI-9. </h3>
+                    </div>
+                    <div className={`col-md-4 pt-5 pb-5`}>
+                        <img src={`/images/about.png`} />
                     </div>
                 </div>
                 <div className={`row pb-5`}>
